bcb40fdf1d761cc37c63cf71cfb92c7554edc30c
Tomas <tomas.mora1@ulatina.net>
2024-10-16 20:57:06 -0600

    Arreglando errores de la navegación con los tabs

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function iniciarApp() {
+    mostrarSeccion(); // Muestra y oculta las secciones
     tabs(); // Cambia la sección cuando se presionen los tabs
 }
 
@@ -20,11 +21,21 @@ function mostrarSeccion() {
     const pasoSelector = `#paso-${paso}`;
     const seccion = document.querySelector(pasoSelector);
     seccion.classList.add('mostrar');
+
+    // Quita la clase de actual al tab anterior
+    const tabAnterior = document.querySelector('.actual');
+    if (tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+
+    // Resaltar el tab actual
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add('actual');
 }
 
 function tabs() {
+    // Agrega y cambia la variable según el tab seleccionado
     const botones = document.querySelectorAll('.tabs button');
-
     botones.forEach( boton => {
         boton.addEventListener('click', function(e) {
             paso = parseInt(e.target.dataset.paso);
