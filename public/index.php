d545d458df3756cf63c902f2e8053e681ceeea0f
Tomas <tomas.mora1@ulatina.net>
2024-10-16 19:15:41 -0600

    Creando routing y Controllers para Citas

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/app.php';
 
 //use Controllers\LoginController;
 
+use Controllers\CitaController;
 use Controllers\LoginController;
 use MVC\Router;
 
@@ -28,6 +29,9 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']);
 // Confirmar Cuenta
 $router->get('/confirmar-cuenta', [LoginController::class, 'confirmar']);
 $router->get('/mensaje', [LoginController::class, 'mensaje']);
+
+// Area Privada
+$router->get('/cita', [CitaController::class, 'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
