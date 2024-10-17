44a30912778b5cb38513c4f5e27b350f01dc3c54
Tomas <tomas.mora1@ulatina.net>
2024-10-16 19:42:14 -0600

    Añadiendo html de citas y servicios y mostrando el nombre del usuario autenticado

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,2 +1,47 @@
 <h1 class="nombre-pagina">Crear Nueva Cita</h1>
-<p class="descripcion-pagina">Elige tus servicios a continuación</p>
+<p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
+
+<div id="app">
+    <div id="paso-1" class="seccion">
+        <h2>Servicios</h2>
+        <p class="text-center">Elige tus servicios a continuación</p>
+        <div id="servicios" class="listado-servicios"></div>
+    </div>
+    <div id="paso-2" class="seccion">
+        <h2>Tus Datos y Cita</h2>
+        <p class="text-center">Coloca la fecha y hora de tu cita</p>
+
+        <form class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input 
+                    id="nombre"
+                    type="text"
+                    placeholder="Tu Nombre"
+                    value="<?php echo $nombre; ?>"
+                    disabled
+                />
+            </div>
+
+            <div class="campo">
+                <label for="fecha">Fecha</label>
+                <input 
+                    id="fecha"
+                    type="date"
+                />
+            </div>
+
+            <div class="campo">
+                <label for="hora">Hora</label>
+                <input 
+                    id="hora"
+                    type="time"
+                />
+            </div>
+        </form>
+    </div>
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p class="text-center">Verifica que la información sea correcta</p>
+    </div>
+</div>
