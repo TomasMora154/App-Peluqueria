a0503729cb71f0ac94139221d92b00cc35f1cc90
Tomas <tomas.mora1@ulatina.net>
2024-11-11 17:29:44 -0600

    Enviar datos del form y añadir los servicion con fetch

--- src/js/app.js
+++ src/js/app.js
@@ -337,6 +337,27 @@ function mostrarResumen() {
     resumen.appendChild(botonReservar);
 }
 
-function reservarCita() {
-    console.log('Reservando Cita...')
+async function reservarCita() {
+
+   const { nombre, fecha, hora, servicios } = cita;
+
+   const idServicios = servicios.map( servicio => servicio.id );
+
+   const datos = new FormData();
+   datos.append('nombre', nombre);
+   datos.append('fecha', fecha);
+   datos.append('hora', hora);
+   datos.append('servicios', idServicios);
+
+   // Petición hacia la API
+   const url = 'http://localhost:3000/api/citas'
+   const respuesta = await fetch(url, {
+        method: 'POST',
+        body: datos
+   });
+
+   const resultado = await respuesta.json();
+   console.log(resultado);
+
+   // console.log(...[datos])
 }
